509ea7bde1f3ba6a4108fe7f7b04e138b2a74232
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2023-03-29 10:18:41 -0400

    vue 1

---
+++ vue-reactivity/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="title"><h1>Album Shop</h1></div>
+</template>
+
+<script>
+export default {
+  albums: [
+    { name: "Ready To Be", group: "Twice", price: "$25.99" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+    { name: "", group: "", price: "" },
+  ],
+};
+</script>
+
+<style lang="scss" scoped></style>
