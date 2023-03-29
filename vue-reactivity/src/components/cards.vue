71d73fc01563d7c59bfbd25f06943c29beb00717
MichelleO3O <mchao9406@gmail.com>
2023-03-29 14:45:29 -0400

    vue 2

---
+++ vue-reactivity/src/components/cards.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="product">
+    <img :src="props.img" :alt="props.name" />
+    <h3>{{ props.name }}</h3>
+    <h4>{{ props.group }}</h4>
+    <h5>{{ props.price }}</h5>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  name: String,
+  group: String,
+  price: String,
+  img: String,
+});
+</script>
--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="title"><h1>Album Shop</h1></div>
+  <cards
+    v-for="(album, index) in albums"
+    :name="album.name"
+    :group="album.group"
+    :price="album.price"
+    :img="album.img"
+  >
+  </cards>
 </template>
 
-<script>
-export default {
-  albums: [
-    { name: "Ready To Be", group: "Twice", price: "$25.99" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-    { name: "", group: "", price: "" },
-  ],
-};
+<script setup>
+import cards from "../components/cards.vue";
+
+const albums = [
+  { name: "Ready To Be", group: "Twice", price: "$25.99", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+  { name: "", group: "", price: "", img: "" },
+];
 </script>
 
 <style lang="scss" scoped></style>
